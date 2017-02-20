0c71ca60011e0bd21444d54c33bac4f2446d4ccb
lylogi <ngly58@gmail.com>
2017-02-20 09:08:08 +0700

    search book for user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,9 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.order_by_time.paginate page: params[:page], per_page: Settings.per_page
+    @categories = Category.all
+    @books_search = Book.search(params[:search], params[:category_id])
+      .paginate page: params[:page], per_page: Settings.per_page
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -12,4 +12,19 @@ class Book < ApplicationRecord
   validates :publish_date, presence: true
 
   scope :order_by_time, -> {order created_at: :desc}
+  scope :order_by_id, -> {order id: :desc}
+  scope :search_book_by_search_params, -> search do
+    where "title LIKE ? OR author_name LIKE ?",
+      "%#{search}%", "%#{search}%" if search.present?
+  end
+  
+  class << self	
+    def search search, category_id
+      if category_id.present?
+        Category.find_by(id: category_id).books
+      else
+        all
+      end.search_book_by_search_params(search)
+    end
+  end
 end
